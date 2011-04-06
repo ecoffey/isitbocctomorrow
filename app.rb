1c28b23b020de0d9867b7051e264efca42af6231
Eoin Coffey <ecoffey@gmail.com>
2011-04-05 23:03:54 -0600

    cleaning some stuff up

--- app.rb
+++ app.rb
@@ -4,15 +4,16 @@ require 'active_support/time'
 
 module IsItBoccTomorrow
   class Application < Sinatra::Base
-    seed = DateTime.new(2011, 03, 29)
     enable :run
 
+    seed = DateTime.new(2011, 03, 29)
+
     get '/' do
-      now = Time.now.utc.in_time_zone("Mountain Time (US & Canada)").to_date
+      tomorrow = Time.now.tomorrow.utc.in_time_zone("Mountain Time (US & Canada)").to_date
 
-      days = (now - seed).to_i + 1
+      diff_days = (tomorrow - seed).to_i
 
-      @is_bocc_tomorrow = days % 14 == 0 ? "yes" : "no"
+      @is_bocc_tomorrow = diff_days % 14 == 0 ? "yes" : "no"
 
       haml :index
     end
