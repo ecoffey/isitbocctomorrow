c9d2f154876d370df05213e6067857e098db4d6f
Eoin Coffey <ecoffey@gmail.com>
2011-04-07 19:33:25 -0600

    now displaying previous / next bocc date

--- app.rb
+++ app.rb
@@ -6,15 +6,23 @@ module IsItBoccTomorrow
   class Application < Sinatra::Base
     enable :run
 
-    seed = DateTime.new(2011, 03, 29)
+    seed = DateTime.new(2011, 03, 15)
 
     get '/' do
       response.headers['Cache-Control'] = 'public, max-age=600'
+
       tomorrow = Time.now.tomorrow.utc.in_time_zone("Mountain Time (US & Canada)").to_date
 
-      diff_days = (tomorrow - seed).to_i
+      @diff_days = (tomorrow - seed).to_i % 14
 
-      @is_bocc_tomorrow = diff_days % 14 == 0 ? "yes" : "no"
+      last_bocc = tomorrow - @diff_days
+      next_bocc = last_bocc.advance(:days => 14)
+
+      @days_till_next_bocc = (next_bocc - tomorrow).to_i + 1
+
+      @last_bocc = last_bocc.to_formatted_s(:long_ordinal)
+      @next_bocc = next_bocc.to_formatted_s(:long_ordinal)
+      @is_bocc_tomorrow = @diff_days == 0 ? "yes" : "no"
 
       haml :index
     end
