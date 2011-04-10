9aaf3cc2d98deccfd18a7d58bac35e9f049a5b06
Eoin Coffey <ecoffey@gmail.com>
2011-04-10 12:07:28 -0600

    diff_days didn't need to be a class variable

--- app.rb
+++ app.rb
@@ -12,16 +12,16 @@ module IsItBoccTomorrow
       now = Time.now.utc.in_time_zone("Mountain Time (US & Canada)")
       tomorrow = now.tomorrow.to_date
 
-      @diff_days = (tomorrow - seed).to_i % 14
+      diff_days = (tomorrow - seed).to_i % 14
 
-      last_bocc = tomorrow - @diff_days
+      last_bocc = tomorrow - diff_days
       next_bocc = last_bocc.advance(:days => 14)
 
       @days_till_next_bocc = (next_bocc - tomorrow).to_i + 1
 
       @last_bocc = last_bocc.to_formatted_s(:long_ordinal)
       @next_bocc = next_bocc.to_formatted_s(:long_ordinal)
-      @is_bocc_tomorrow = @diff_days == 0 ? "yes" : "no"
+      @is_bocc_tomorrow = diff_days == 0 ? "yes" : "no"
 
       haml :index
     end
