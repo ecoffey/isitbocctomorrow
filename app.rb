c751f272c15bbfa2eed396170675a42eedb51d87
Eoin Coffey <ecoffey@gmail.com>
2011-04-11 10:17:42 -0600

    bug with calculating last / next bocc. now based on today instead of tomorrow

--- app.rb
+++ app.rb
@@ -9,19 +9,20 @@ module IsItBoccTomorrow
     seed = DateTime.new(2011, 03, 15)
 
     get '/' do
-      now = Time.now.utc.in_time_zone("Mountain Time (US & Canada)")
+      now = Time.now.utc.in_time_zone("Mountain Time (US & Canada)").to_date
       tomorrow = now.tomorrow.to_date
 
-      diff_days = (tomorrow - seed).to_i % 14
+      diff_days_now = (now - seed).to_i % 14
+      diff_days_tomorrow = (tomorrow - seed).to_i % 14
 
-      last_bocc = tomorrow - diff_days
+      last_bocc = now - diff_days_now
       next_bocc = last_bocc.advance(:days => 14)
 
-      @days_till_next_bocc = (next_bocc - tomorrow).to_i + 1
+      @days_till_next_bocc = (next_bocc - now).to_i
 
       @last_bocc = last_bocc.to_formatted_s(:long_ordinal)
       @next_bocc = next_bocc.to_formatted_s(:long_ordinal)
-      @is_bocc_tomorrow = diff_days == 0 ? "yes" : "no"
+      @is_bocc_tomorrow = diff_days_tomorrow == 0 ? "yes" : "no"
 
       haml :index
     end
